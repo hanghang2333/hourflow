2d3736d1a41893bbb3b62cf8ce23c3f88f6ba9cb
lihang <1092978787@qq.com>
2018-02-09 10:13:15 +0800

    增加了卷积操作conv2d和maxpooling

--- Operation/operations.py
+++ Operation/operations.py
@@ -56,3 +56,85 @@ class negative(Operation):
     
     def compute(self,x_value):
         return -x_value
+
+class flatten(Operation):
+    def __init__(self,x):
+        super().__init__([x])
+    def compute(self,x_value):
+        #print('d',x_value)
+        return np.reshape(x_value,[x_value.shape[0],-1]) 
+
+class conv2d(Operation):
+    def __init__(self,x,w,b, strides=1, pad='valid'):
+        #    H' = 1 + (H + 2 * pad - HH) / stride
+        #    W' = 1 + (W + 2 * pad - WW) / stride  
+        super().__init__([x,w,b])
+        self.strides = strides
+        self.pad = pad
+    def compute(self,x,w,b):
+        N,C,H,W = x.shape#10*224*224*3
+        F,_,HH,WW = w.shape#6*3*3*3
+        S = self.strides
+        PH,PW = 0,0
+        Ho,Wo = int(1+(H-HH)/S),int(1+(W-WW)/S)
+        x_pad = None
+        if self.pad == 'valid':
+            x_pad = np.zeros((N,C,H,W))
+            x_pad[:,:,0:H,0:W]=x
+        else:
+            PH = (H-1)*S+HH-H
+            PW = (W-1)*S+WW-W#当求出来需要pad的数目是奇数时就不好处理，这里选择右边多pad一个
+            if PH%2==0 and PW%2==0:
+                x_pad = np.zeros((N,C,H+PH,W+PW))
+                #print(PH/2,PH/2+H,PW/2,PW/2+W)
+                x_pad[:,:,int(PH/2):int(PH/2)+H,int(PW/2):int(PW/2)+W]=x                
+            elif PH%2==0 and PW%2==1:
+                x_pad = np.zeros((N,C,H+PH,W+PW))
+                x_pad[:,:,int(PH/2):int(PH/2)+H,int(PW/2):int(PW/2)+W]=x
+            elif PH%2==1 and PW%2==0:
+                x_pad = np.zeros((N,C,H+PH,W+PW))
+                x_pad[:,:,int(PH/2):int(PH/2)+H,int(PW/2):int(PW/2)+W]=x
+            else:
+                x_pad = np.zeros((N,C,H+PH,W+PW))
+                x_pad[:,:,int(PH/2):int(PH/2)+H,int(PW/2):int(PW/2)+W]=x                                
+            Ho,Wo = H,W
+        #x_pad = np.pad(x, ((0,), (0,), (P,), (P,)), 'constant')
+        out = np.zeros((N,F,Ho,Wo))
+        for f in range(F):
+            for i in range(Ho):
+                for j in range(Wo):
+                # N*C*HH*WW, C*HH*WW = N*C*HH*WW, sum -> N*1
+                    out[:,f,i,j] = np.sum(x_pad[:, :, i*S : i*S+HH, j*S : j*S+WW] * w[f, :, :, :], axis=(1, 2, 3)) 
+            out[:,f,:,:]+=b[f]
+        return out
+
+class pooling(Operation):
+    def __init__(self,x,size=2,stride=2,mode='max'):
+        #目前只可以使用maxpooling
+        super().__init__([x])
+        self.size = size
+        self.stride = stride
+        self.mode = mode
+    def compute(self,x):
+        batch,cha,in_row,in_col = np.shape(x)
+        # outputMap sizes
+        out_row,out_col = int(np.floor(in_row/self.stride)),int(np.floor(in_col/self.stride))
+        row_remainder,col_remainder = np.mod(in_row,self.stride),np.mod(in_col,self.stride)
+        if row_remainder != 0:
+            out_row +=1
+        if col_remainder != 0:
+            out_col +=1
+        outputMap = np.zeros((batch,cha,out_row,out_col))
+        # padding
+        temp_map = np.lib.pad(x, ((0,0),(0,0),(0,self.size-row_remainder),(0,self.size-col_remainder)),'edge')
+        # max pooling
+        for batchi in range(batch):
+            for chai in range(cha):
+                for r_idx in range(0,out_row):
+                    for c_idx in range(0,out_col):
+                        startX = c_idx * self.stride
+                        startY = r_idx * self.stride
+                        poolField = temp_map[batchi,chai,startY:startY + self.size, startX:startX + self.size]
+                        poolOut = np.max(poolField)
+                        outputMap[batchi,chai,r_idx,c_idx] = poolOut
+        return  outputMap
--- optimizer.py
+++ optimizer.py
@@ -98,3 +98,89 @@ def _reduce_sum_gradient(op,grad):
 def _softmax_gradient(op,grad):
     softmax = op.output
     return (grad-np.reshape(np.sum(grad*softmax,-1),[-1,1]))*softmax
+
+@RegisterGradient("flatten")
+def _flatten_gradient(op,grad):
+    return np.reshape(grad,op.inputs[0].shape)
+
+@RegisterGradient("conv2d")
+def _conv2d_gradient(op, grad):
+    x,w,b = op.inputs[0],op.inputs[1],op.inputs[2]
+    dout = grad
+    #print(grad)
+    N, F, H1, W1 = dout.shape
+    N, C, H, W = x.shape
+    HH = w.shape[2]
+    WW = w.shape[3]
+    S = op.strides
+    pad = op.pad
+    PH,PW = 0,0
+    Ho,Wo = int(1+(H-HH)/S),int(1+(W-WW)/S)
+    x_pad = None
+    dx_pad = None
+    if pad == 'valid':
+        x_pad = np.zeros((N,C,H,W))
+        dx_pad = np.zeros((N,C,H,W))
+        x_pad[:,:,0:H,0:W]=x
+    else:
+        PH = (H-1)*S+HH-H
+        PW = (W-1)*S+WW-W#当求出来需要pad的数目是奇数时就不好处理，这里选择右边多pad一个
+        if PH%2==0 and PW%2==0:
+            x_pad = np.zeros((N,C,H+PH,W+PW))
+            x_pad[:,:,PH/2:PH/2+H,PW/2:PW/2+W]=x                
+        elif PH%2==0 and PW%2==1:
+            x_pad = np.zeros((N,C,H+PH,W+PW))
+            x_pad[:,:,PH/2:PH/2+H,int(PW/2):int(PW/2)+W]=x
+        elif PH%2==1 and PW%2==0:
+            x_pad = np.zeros((N,C,H+PH,W+PW))
+            x_pad[:,:,int(PH/2):int(PH/2)+H,PW/2:PW/2+W]=x
+        else:
+            x_pad = np.zeros((N,C,H+PH,W+PW))
+            x_pad[:,:,int(PH/2):int(PH/2)+H,int(PW/2):int(PW/2)+W]=x                                
+        Ho,Wo = H,W
+    dx, dw, db = np.zeros_like(x), np.zeros_like(w), np.zeros_like(b)
+    dx_pad = np.zeros((N,C,H+PH,W+PW))
+    db = np.sum(dout, axis=(0,2,3))
+    for n in range(N):
+        for i in range(H1):
+            for j in range(W1):
+            # Window we want to apply the respective f th filter over (C, HH, WW)
+                x_window = x_pad[n, :, i * S : i * S + HH, j * S : j * S + WW]
+                for f in range(F):
+                    dw[f] += x_window * dout[n, f, i, j]
+                    dx_pad[n, :, i * S : i * S + HH, j * S : j * S + WW] += w[f] * dout[n, f, i, j]
+    dx = dx_pad[:, :, PH:PH+H, PW:PW+W]
+    return [dx, dw, db]
+
+@RegisterGradient("pooling")
+def _pooling_gradient(op,grad):
+    size = op.size
+    stride = op.stride
+    mode = op.mode
+    x = op.inputs[0]
+    batch,cha,in_row,in_col = np.shape(x)
+    # outputMap sizes
+    out_row,out_col = int(np.floor(in_row/stride)),int(np.floor(in_col/stride))
+    row_remainder,col_remainder = np.mod(in_row,stride),np.mod(in_col,stride)
+    if row_remainder != 0:
+        out_row +=1
+    if col_remainder != 0:
+        out_col +=1
+    # padding
+    temp_map = np.lib.pad(x, ((0,0),(0,0),(0,size-row_remainder),(0,size-col_remainder)),'edge')
+    # max pooling
+    out = np.zeros_like(x)
+    for batchi in range(batch):
+        for chai in range(cha):
+            for r_idx in range(0,out_row):
+                for c_idx in range(0,out_col):
+                    startX = c_idx * stride
+                    startY = r_idx * stride
+                    poolField = temp_map[batchi,chai,startY:startY + size, startX:startX + size]
+                    poolOut = np.max(poolField)
+                    index = np.where(poolField==poolOut)
+                    indexx = index[0][0]+startY
+                    indexy = index[1][0]+startX
+                    #print('ss',grad.shape,batchi,chai,out_row,out_col)
+                    out[batchi,chai,indexx,indexy] = grad[batchi][chai][r_idx][c_idx]
+    return  out
